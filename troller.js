e33cc948ca1c8f49f0c07be4812d4743112a17ed
Student <student@RU-103.local>
2015-09-17 15:00:43 -0600

    infinite button implemented, form hide/show needs work

--- troller.js
+++ troller.js
@@ -1,5 +1,29 @@
 angular.module('infiniteApp', []);
 
 angular.module('infiniteApp').controller('troller', ['$scope', function($scope){
+	$scope.dateArray = [];
+	dateArrayIndex = 0;
 	
-}])
+	var populateDateArray = function(indexSeed) {
+		index = indexSeed;
+		for (var i=index; i<(index+7); i++) {
+			var todaysDate = new Date();
+			futureDate = todaysDate.setDate(todaysDate.getDate() + i);
+			futureDateObject = {
+				date      : new Date(futureDate).toDateString(),
+				showValue : false,
+			}
+			$scope.dateArray.push(futureDateObject);
+			dateArrayIndex++;
+		}
+	}
+
+	$scope.loadMore = function() {
+		populateDateArray(dateArrayIndex);
+	};
+
+	$scope.showDetails = function(index) {
+		$scope.dateArray[index].showValue = !$scope.dateArray[index].showValue;
+	}
+
+}]);
